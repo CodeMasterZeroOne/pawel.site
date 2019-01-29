a2aca6c43e8761cee598ee510329c21cec74f507
P G <pawel.ict.student@gmail.com>
2019-01-29 21:08:34 +0000

    plus

---
+++ socket.js
@@ -0,0 +1,32 @@
+function webSocket() {
+            
+    if ("WebSocket" in window) {
+       alert("WebSocket is supported by your Browser!");
+       
+       // Let us open a web socket
+       var ws = new WebSocket("ws://echo.websocket.org");
+        
+       ws.onopen = function() {
+          
+          // Web Socket is connected, send data using send()
+          ws.send("Message from websocket: Pawel is saying Hello World!");
+          alert("Message is sent...");
+       };
+        
+       ws.onmessage = function (evt) { 
+          var received_msg = evt.data;
+          var msgOutput = document.getElementById("socketOutput");
+          msgOutput.innerHTML = received_msg;
+       };
+        
+       ws.onclose = function() { 
+          
+          // websocket is closed.
+          //alert("Connection closed.");
+       };
+    } else {
+      
+       // The browser doesn't support WebSocket
+       alert("WebSocket NOT supported by your Browser!");
+    }
+ }
